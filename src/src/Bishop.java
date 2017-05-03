b83419033c601f78d0e0f050918e9a7662b85b0a
AndrewJOwens <andrew.owens@my.uwrf.edu>
2017-05-03 11:38:17 -0500

    Big ol update

--- src/src/Bishop.java
+++ src/src/Bishop.java
@@ -1,22 +1,36 @@
 package src;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Bishop extends Piece {
 	
 	public Bishop(int x, int y, char side, PieceType type, int i) {
 		super(x, y, side, type, i);
-		setImage(getSide());
-		
-	}
-	private String getSide() {
-		String path = null;
-		if (side == 'w') {
-			path = "C:\\Users\\Yam\\workspace\\Chess\\src\\piecePictures\\white_bishop.png";
-		} 
-		if (side == 'b') {
-			path = "C:\\Users\\Yam\\workspace\\Chess\\src\\piecePictures\\black_bishop.png";
+		try {
+			setImage(getSide());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		return path;
+	}
+	private String getSide() throws IOException {
+		String path = null;
+		String finalPath = null;
+		File currentDir = new File(".");
+		String basePath = currentDir.getCanonicalPath();
+
+		if (side == 'w') {
+			path = "\\src\\piecePictures\\white_bishop.png";
+			finalPath = basePath.concat(path);
+		} 
+		if (side == 'b') {
+			path = "\\src\\piecePictures\\black_bishop.png";
+			finalPath = basePath.concat(path);
+		}
+		
+		return finalPath;
 	}
 	
 }
