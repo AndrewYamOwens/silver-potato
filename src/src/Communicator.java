54b947fd0973305a244080067b5a30e17d5a4ae0
andrew <andrew@Yams-Laptop>
2017-05-03 21:51:25 -0500

    clientMaster 0.1

--- src/src/Communicator.java
+++ src/src/Communicator.java
@@ -24,7 +24,7 @@ public class Communicator {
 	
 	public void startConnection() {
 		try {
-			hostConnect();
+			clientConnect();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
